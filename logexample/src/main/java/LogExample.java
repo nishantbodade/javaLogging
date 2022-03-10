6cb1a8e51dbf5549ddb8f59a1de307ff62df7204
Nishant Bodade <nishantbodade@gmail.com>
2022-03-10 16:11:11 +0530

    clean code best practices
    mention who,when,where,what,and the result

--- logexample/src/main/java/LogExample.java
+++ logexample/src/main/java/LogExample.java
@@ -5,13 +5,16 @@ import java.util.logging.Logger;
 public class LogExample {
 
     private static final Logger LOGGER=Logger.getLogger(LogExample.class.getName());
-    public static void main(String[] args) {
-        LOGGER.setLevel(Level.FINEST);
 
+    static
+    {
+        LOGGER.setLevel(Level.FINEST);
         ConsoleHandler consoleHandler=new ConsoleHandler();
         consoleHandler.setLevel(Level.FINEST);
-
         LOGGER.addHandler(consoleHandler);
+
+    }
+    public static void main(String[] args) {
 
         LOGGER.log(Level.FINEST,"this is an example message");
 
