f7e4d353e7436e404025042e0cbb62ea53879725
Nishant Bodade <nishantbodade@gmail.com>
2022-03-10 18:19:30 +0530

    LogManager added
    properties file added for logger
    
    UTIL class is added for LogManager

--- logexample/src/main/java/LogExample.java
+++ logexample/src/main/java/LogExample.java
@@ -1,32 +1,19 @@
+import utils.LoggingUtil;
+
 import java.io.IOException;
 import java.util.logging.*;
 
 public class LogExample {
 
+
     private static final Logger LOGGER=Logger.getLogger(LogExample.class.getName());
 
-    static
-    {
-        LOGGER.setLevel(Level.FINEST);
-        FileHandler fileHandler= null;
-        try {
-            fileHandler = new FileHandler(LogExample.class.getName()+".log");
-            fileHandler.setFormatter(new SimpleFormatter());
-            fileHandler.setLevel(Level.FINEST);
-//            Filter filterAll=s ->false;
-////            fileHandler.setFilter(filterAll);//will ignore the logs in file handler only,console will be shown
-////            LOGGER.setFilter(filterAll);//igore all logs including console
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        LOGGER.addHandler(fileHandler);
+    public static void main(String[] args) throws  IOException{
+        LoggingUtil.initLogManager();
 
-    }
-    public static void main(String[] args) {
-
-        LOGGER.log(Level.FINEST,"this is an example message");
+        LOGGER.log(Level.INFO,"this is an example message");
 
 
     }
